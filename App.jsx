f8677e4aa39aad1a4d43933ccdb1059f7d2e6569
Fern <FernandoFlores.private@gmail.com>
2023-09-27 23:40:53 -0400

    project setup with tailwind css config

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home />
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
